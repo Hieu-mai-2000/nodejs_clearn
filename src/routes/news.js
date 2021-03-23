93f460d6c5c6081f64bf61d1eaa04c42a764c822
Hieu-mai-2000 <hieu.mainguyenminh@hcmut.edu.vn>
2021-03-23 10:30:46 +0700

    test

--- src/routes/news.js
+++ src/routes/news.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const newsController = require('../app/controller/NewsController');
 
-            router.get('/:slug', newsController.index);
+router.get('/:slug', newsController.index);
 router.get('/', newsController.main);
 
 module.exports = router;
